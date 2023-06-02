9d73d973a5c07e4e950f29f87620034550eb6e7d
Hayk Vardanyan <hvardanyan94work@gmail.com>
2023-06-02 15:25:43 +0400

    table added

--- bin/index.js
+++ bin/index.js
@@ -3,13 +3,21 @@ import { arch, cpuUsage,exit} from 'node:process';
 import os from "os";
 import { rename, readdir, statSync, writeFile, copyFile, unlink} from "fs"
 import { log } from 'node:console';
+import Table from "cli-table3"
 
 // const userName = os.userInfo().username;
 
 // process.on("message", console.log);
 // process.emit("message", (`Wellcome ${userName}!`))
 
-console.log(process.argv);
+
+// var table = new Table();
+
+var table = new Table({
+    head: ['(index)', 'name', "type"], 
+    colWidths: [10, 20, 15]
+});
+
 
 switch (process.argv[2]) {
     case "ls":
@@ -18,22 +26,17 @@ switch (process.argv[2]) {
             const newFiles = []
            files.forEach( (file) => {
             if (statSync(file).isDirectory()){
-                folders.push({
-                    fileName: file,
-                    fileType: "directory"
-                })
+                folders.push([file,"directory"])
             } else {
-                newFiles.push({
-                    fileName: file,
-                    fileType: "file" 
-                })
+                newFiles.push([file, "file"])
             }
             files = folders.concat(newFiles)
         })  
             files.forEach((file, i) => {
-                file = {i: i, ...file};
-                console.log(Object.values(file));
+                file = [ i, ...file];
+                table.push(file)
             })
+            console.log(table.toString());
         })  
         break;
     case "add":
